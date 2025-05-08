087f5308a7aa669455cfb231a1be7cbcc8b3139b
Collins.rutto <collinsrutto1@gmail.com>
2025-05-08 19:38:31 +0100

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    // Add your custom rules here
+  },
+};
